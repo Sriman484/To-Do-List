30c5b7e82ec320947c39e3a8329d09f5ad89e569
Sriman484 <srimang484@gmail.com>
2025-02-04 21:44:45 +0530

    media query updated

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ var inp = document.getElementById("input");
 
 function add() {
     var listItem = document.createElement("li");
-    listItem.innerHTML = inp.value + "<input type='checkbox' onclick='del(event)'>";
-    ul.append(listItem);
+
+    var textSpan = document.createElement("span");
+    textSpan.textContent = inp.value;
+
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.onclick = del;
+
+    listItem.appendChild(textSpan);
+    listItem.appendChild(checkbox);
+    ul.appendChild(listItem);
+
+    inp.value = "";
 }
+
+
 
 function del(event) {
     event.target.parentElement.style.textDecoration = "line-through";
